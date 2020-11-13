ab2e5fa82ee246fb1e855f18e1d579722095ebf9
minamikawa1990 <ut.magnon@gmail.com>
2020-11-13 12:26:16 +0900

    出力値を縦に表示するのができていない。それ以外は完成

--- main.js
+++ main.js
@@ -7,35 +7,33 @@
 
             const input_FizzNum = parseFloat(document.getElementById("FizzNum").value);//数値を取得
             const input_BuzzNum = parseFloat(document.getElementById("BuzzNum").value);//数値を取得
-            const outputNode = document.getElementById('output');
             const fbArray = [];
             
-            
             if(Number.isInteger(input_FizzNum && input_BuzzNum) === true){
-                for(let i = 1; i < 101; i++){
-                    if(i % input_fizzNum === 0 && i % input_buzzNum === 0) {
+                for(let i = 1; i < 99; i++){
+                    if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
                         fbArray.push('FizzBuzz' + i);
-                    } else if(i % input_fizzNum === 0) {
+                    } else if(i % input_FizzNum === 0) {
                         fbArray.push('Fizz' + i);
-                    } else if(i % input_buzzNum === 0) {
+                    } else if(i % input_BuzzNum === 0) {
                         fbArray.push('Buzz' + i);
                     } 
                 }
             } else {
                 document.write("整数値ではありません");
             }
-
+        
             const p = document.createElement('p');
             p.textContent = fbArray;
-            document.querySelector('p').appendChild(p);
-
-            
+            document.body.appendChild(p);
         }
+        
+
+           
+        
 
         document.querySelector('button').addEventListener('click', () => {
-            outputNode.textContent = judge();
+            judge();
         }); 
 
-        
-       
 }
