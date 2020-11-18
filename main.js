c59752633bc5a41222fe3d8bee49777ee43002ab
minamikawa1990 <ut.magnon@gmail.com>
2020-11-18 21:38:30 +0900

    cssを修正

--- main.js
+++ main.js
@@ -7,60 +7,35 @@
 
             const input_FizzNum = parseFloat(document.getElementById("FizzNum").value);//数値を取得
             const input_BuzzNum = parseFloat(document.getElementById("BuzzNum").value);//数値を取得
-            const fbArray = [];
+            
             
             if(Number.isInteger(input_FizzNum && input_BuzzNum) === true){
                 for(let i = 1; i < 99; i++){
                     if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
-                        const number = document.write('FizzBuzz' + i)
                         const p = document.createElement('p');
+                        p.textContent = 'FizzBuzz' + i;
                         const body = document.querySelector('body');
                         body.appendChild(p);
                     } else if(i % input_FizzNum === 0) {
-                        const number = document.write('Fizz' + i)
                         const p = document.createElement('p');
+                        p.textContent = 'Fizz' + i;
                         const body = document.querySelector('body');
                         body.appendChild(p);
                     } else if(i % input_BuzzNum === 0) {
-                        const number = document.write('Buzz' + i)
                         const p = document.createElement('p');
+                        p.textContent = 'Buzz' + i;
                         const body = document.querySelector('body');
                         body.appendChild(p);
                     } 
                 }
             } else {
-                document.write("整数値ではありません");
+                const p = document.createElement('p');
+                p.textContent = '整数値を入力してください';
+                const body = document.querySelector('body');
+                body.appendChild(p);
             }
-            // if(Number.isInteger(input_FizzNum && input_BuzzNum) === true){
-            //     for(let i = 1; i < 99; i++){
-            //         if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
-            //             fbArray.push('FizzBuzz' + i);
-            //         } else if(i % input_FizzNum === 0) {
-            //             fbArray.push('Fizz' + i);
-            //         } else if(i % input_BuzzNum === 0) {
-            //             fbArray.push('Buzz' + i);
-            //         } 
-            //     }
-            // } else {
-            //     document.write("整数値ではありません");
-            // }
             
             
-            
-            // p.textContent = fbArray;
-            p.textContent = fbArray.join('');
-            
-            
-            
-            
-
-             // for(const v of fbArry) {
-            //     fbText.push(`<p>${v}</p>`);
-            // }
-
-            // const fbHTML = fbText.join('');
-
-            // fbResult.innerHTML = fbHTML;
         }
 
 
