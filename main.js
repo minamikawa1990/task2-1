442edb86e99803e99eac8122a2fe95faecc0040e
minamikawa1990 <ut.magnon@gmail.com>
2020-11-23 12:55:43 +0900

    課題修正済み

--- main.js
+++ main.js
@@ -8,44 +8,46 @@
             const input_FizzNum = parseFloat(document.getElementById("FizzNum").value);//数値を取得
             const input_BuzzNum = parseFloat(document.getElementById("BuzzNum").value);//数値を取得
             
-            
-            if(Number.isInteger(input_FizzNum && input_BuzzNum) === true){
-                for(let i = 1; i < 99; i++){
-                    if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
-                        const p = document.createElement('p');
-                        p.textContent = 'FizzBuzz' + i;
-                        const body = document.querySelector('body');
-                        body.appendChild(p);
-                    } else if(i % input_FizzNum === 0) {
-                        const p = document.createElement('p');
-                        p.textContent = 'Fizz' + i;
-                        const body = document.querySelector('body');
-                        body.appendChild(p);
-                    } else if(i % input_BuzzNum === 0) {
-                        const p = document.createElement('p');
-                        p.textContent = 'Buzz' + i;
-                        const body = document.querySelector('body');
-                        body.appendChild(p);
-                    } 
-                }
-            } else {
+            if(!Number.isInteger(input_FizzNum && input_BuzzNum)){
                 const p = document.createElement('p');
+                p.classList.add ='output';
                 p.textContent = '整数値を入力してください';
                 const body = document.querySelector('body');
                 body.appendChild(p);
+                return;
+            }
+            for(let i = 1; i < 99; i++){
+                if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
+                    const p = document.createElement('p');
+                    p.classList.add('output');
+                    p.textContent = 'FizzBuzz' + i;
+                    const body = document.querySelector('body');
+                    body.appendChild(p);    
+                } else if(i % input_FizzNum === 0) {
+                    const p = document.createElement('p');
+                    p.classList.add('output');
+                    p.textContent = 'Fizz' + i;
+                    const body = document.querySelector('body');
+                    body.appendChild(p);
+                } else if(i % input_BuzzNum === 0) {
+                    const p = document.createElement('p');
+                    p.classList.add('output');
+                    p.textContent = 'Buzz' + i;
+                    const body = document.querySelector('body');
+                    body.appendChild(p);
+                } 
             }
             
-            
         }
-
-
         
-
-           
-        
-
         document.querySelector('button').addEventListener('click', () => {
+            // const p = document.querySelectorAll('p');
+            // if(p.classList.includes('output') === true){
+            //     p.innerHTML = '';
+            // }
+            
             judge();
         }); 
+    
 
 }
