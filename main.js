a39f113dabda83cc146f418763e249fe0aeef1c1
minamikawa1990 <ut.magnon@gmail.com>
2020-12-03 07:55:19 +0900

    課題を実装できた

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// 'use strict';
 
 {
     
-    
         function judge() {
 
-            const input_FizzNum = parseFloat(document.getElementById("FizzNum").value);//数値を取得
-            const input_BuzzNum = parseFloat(document.getElementById("BuzzNum").value);//数値を取得
+            // const ul = document.querySelectorAll('ul');
+
+            // while(ul.firstChild){
+            //     ul.removeChild(ul.firstChild);
+            // }
+            const output = document.getElementById('output');
+            output.innerHTML=" ";
+
+            const input_FizzNum = parseFloat(document.getElementById("FizzNum").value);
+            const input_BuzzNum = parseFloat(document.getElementById("BuzzNum").value);
             
             if(!Number.isInteger(input_FizzNum || input_BuzzNum)){
                 const p = document.createElement('p');
@@ -16,37 +22,66 @@
                 body.appendChild(p);
                 return;
             }
+            // for(let i = 1; i < 99; i++){
+            //     if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
+            //         const p = document.createElement('p');
+            //         p.classList.add('output');
+            //         p.textContent = 'FizzBuzz' + i;
+            //         const body = document.querySelector('body');
+            //         body.appendChild(p);    
+            //     } else if(i % input_FizzNum === 0) {
+            //         const p = document.createElement('p');
+            //         p.classList.add('output');
+            //         p.textContent = 'Fizz' + i;
+            //         const body = document.querySelector('body');
+            //         body.appendChild(p);
+            //     } else if(i % input_BuzzNum === 0) {
+            //         const p = document.createElement('p');
+            //         p.classList.add('output');
+            //         p.textContent = 'Buzz' + i;
+            //         const body = document.querySelector('body');
+            //         body.appendChild(p);
+            //     } 
+            // }
             for(let i = 1; i < 99; i++){
                 if(i % input_FizzNum === 0 && i % input_BuzzNum === 0) {
                     const p = document.createElement('p');
                     p.classList.add('output');
                     p.textContent = 'FizzBuzz' + i;
-                    const body = document.querySelector('body');
-                    body.appendChild(p);    
+                    const output = document.getElementById('output');
+                    output.appendChild(p);    
                 } else if(i % input_FizzNum === 0) {
                     const p = document.createElement('p');
                     p.classList.add('output');
                     p.textContent = 'Fizz' + i;
-                    const body = document.querySelector('body');
-                    body.appendChild(p);
+                    const output = document.getElementById('output');
+                    output.appendChild(p);
                 } else if(i % input_BuzzNum === 0) {
                     const p = document.createElement('p');
                     p.classList.add('output');
                     p.textContent = 'Buzz' + i;
-                    const body = document.querySelector('body');
-                    body.appendChild(p);
+                    const output = document.getElementById('output');
+                    output.appendChild(p);
                 } 
             }
+            
             
         }
         
         document.querySelector('button').addEventListener('click', () => {
-            // const p = document.querySelectorAll('p');
-            // if(p.classList.includes('output') === true){
+            // const p = document.getElementsByClassName('output');
+            // const p = document.querySelectorAll.classList.contains('output');
+           
+            // while(p){
+            //     p.remove();
+            // }
+            // if(p){
             //     p.innerHTML = '';
             // }
+            // console.log(p)
             
             judge();
+            
         }); 
     
 
